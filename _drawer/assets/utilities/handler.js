06bb71da9757a63ff88164347ff7c4a1de5da0a8
M Syamsir Alam <graduatedby@outlook.com>
2024-03-07 11:06:34 +0800

    #3 create README.md

---
+++ _drawer/assets/utilities/handler.js
@@ -0,0 +1,12 @@
+/* handler.js file was authored by M. Syamsir.Alam (syamsiralam.id@gmail.com) for such mathematical software & factories (www.abemath.com). */
+/* All of these activity on our responsible and code or all of my property which privately coded already in creative common license. */
+/* Any use can be allowed without permission or permission itself. */
+
+	function test() {
+
+		let text = "My name is";
+		return text;
+	
+};
+	
+	export { test}
--- public_html/_graph/assets/assets.js
+++
@@ -1,8 +0,0 @@
-
-gl.vertexAttrib3f(position, 0.25, 0, 0);
-gl.uniform4f(color, 0, 1, 0, 1); // green
-gl.drawArrays(gl.POINTS, 0, 1);
-
-gl.vertexAttrib3f(position, 0.5, 0, 0);
-gl.uniform4f(color, 0, 0, 1, 1); // blue
-gl.drawArrays(gl.POINTS, 0, 1);
